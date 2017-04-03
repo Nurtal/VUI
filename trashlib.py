9b54452782cb974c7e0e7d044c3851d59befc0eb
PC_immuno <PC_immuno>
2017-04-03 17:26:28 +0200

    check progress

--- trashlib.py
+++ trashlib.py
@@ -1,9 +1,21 @@
 import pyttsx
 import datetime
+import psutil
 
 #
 # Have to install pypiwin 32 on windows 
 #
+
+
+def presentation():
+	"""
+	in progress
+	"""
+
+	sentence_1 = "I am a vocal user interface, devlopped for the reduction dimmension project"
+	engine = pyttsx.init()
+	engine.say(sentence_1)
+	engine.runAndWait()
 
 
 
@@ -20,8 +32,28 @@ def get_date():
 	engine.runAndWait()
 
 
+def check_process():
+	"""
+	Check process running
+	
+	TODO:
+		-> look just for the interting process 
+	"""
+
+	engine = pyttsx.init()
+	engine.say("I detect a few process running")
+	process_id_list = psutil.pids()
+	for pid in process_id_list:
+
+		process = psutil.Process(pid)
+		process_status = process.status()
+		process_name = process.name()
+		engine.say(str(process_name)+" is "+str(process_status))
+		
+	engine.runAndWait()
+
+
 # TEST SPACE
-#engine = pyttsx.init()
 
 #for x in range(1,11):
 #	engine.say(str(x) + "rabbits")
@@ -30,4 +62,8 @@ def get_date():
 #engine.say("and rabbits")
 #engine.runAndWait()
 
+presentation()
 get_date()
+check_process()
+
+
